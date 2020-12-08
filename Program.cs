15fcc84ae7521455543ced19399716530d366aea
GabrielaGonzaga <73176744+GabrielaGonzaga@users.noreply.github.com>
2020-12-08 09:35:33 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Encapsulamento_8_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Classes.MasterCard m = new Classes.MasterCard();
+            
+            m.Parcelas = 12;
+            
+        
+           
+            
+        }
+    }
+}
